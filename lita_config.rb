e86879ba0b84e45b13bbd2da20f5e6d192e80efb
sharon <1014110874@qq.com>
2020-10-26 18:26:50 +0800

    comment

--- lita_config.rb
+++ lita_config.rb
@@ -56,13 +56,13 @@ end
 if ENV['RACK_ENV']=='production'
   config.robot.adapter = :slack
   config.redis[:url]=ENV.fetch('REDIS_URL')
-  #config.adapters.slack.token = "xoxb-1447072035412-1446112783174-gvpWAglnMpzQrpxYNkCYAWhc"
+  config.adapters.slack.token = "xoxb-1447072035412-1446112783174-gvpWAglnMpzQrpxYNkCYAWhc"
   #config.redis[:url]=ENV.fetch('redis://h:p3b0367716aca36ce25b1779241754684109aa4421e23845e1ad85c3f6d203b13@ec2-23-21-82-81.compute-1.amazonaws.com:11959')
 else
   config.robot.adapter=:shell
 end
 #slack adapter demands a value even in dev when we aren't using it..
-config.adapters.slack.token=ENV.fetch('SLACK_API_KEY','')
+#config.adapters.slack.token=ENV.fetch('SLACK_API_KEY','')
   ## Example: Set configuration for any loaded handlers. See the handler's
   ## documentation for options.
   # config.handlers.some_handler.some_config_key = "value"
